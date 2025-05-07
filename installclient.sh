7e47b4af3ef900ea4ac4e698aa92cf0857de4c84
ItsDev?_ <149790336+itsdev-ssh@users.noreply.github.com>
2025-05-07 12:08:46 -0400

    Update installclient.sh

--- installclient.sh
+++ installclient.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Instala FRP Client (frpc) na máquina local (CGNAT)
+# Instala FRP Client
 FRP_VERSION="0.58.0"
 FRP_DIR="frp_${FRP_VERSION}_linux_amd64"
 DOWNLOAD_URL="https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FRP_DIR}.tar.gz"
